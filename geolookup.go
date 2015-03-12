f3999dd501f4ae229d17bc2a0f6f4b6f5996d265
Rene Fragoso <ctrlrsf@gmail.com>
2015-03-12 09:50:51 -0400

    Bump version and change ipv4address flag to ipv4

--- geolookup.go
+++ geolookup.go
@@ -15,12 +15,14 @@ import (
 
 const countryDbURL = "http://geolite.maxmind.com/download/geoip/database/GeoLite2-Country.mmdb.gz"
 const countryDbOutputFile = "GeoLite2-Country.mmdb"
+const version = "0.0.1"
 
 func main() {
 	app := cli.NewApp()
 	app.Name = "geolookup"
 	app.Author = "Rene Fragoso"
 	app.Email = "ctrlrsf@gmail.com"
+	app.Version = version
 	app.Usage = "Quick tool for querying Maxmind Geo IP database file"
 
 	app.Flags = []cli.Flag{
@@ -30,20 +32,21 @@ func main() {
 		},
 		cli.BoolFlag{
 			Name:  "update",
-			Usage: "Update Maxmind GeoLite2 database",
+			Usage: "Download or update Maxmind GeoLite2 database",
 		},
 		cli.StringFlag{
-			Name: "ipv4Address",
+			Name:  "ipv4",
+			Usage: "Specify IPv4 address to lookup",
 		},
 		cli.StringFlag{
 			Name:  "countrydb",
 			Value: countryDbOutputFile,
-			Usage: "Maxmind database file",
+			Usage: "Maxmind GeoLite2 country DB file",
 		},
 	}
 
 	app.Action = func(c *cli.Context) {
-		ipv4Address := c.String("ipv4Address")
+		ipv4 := c.String("ipv4")
 		countryDb := c.String("countrydb")
 		country := c.Bool("country")
 		update := c.Bool("update")
@@ -53,7 +56,7 @@ func main() {
 		}
 
 		if country {
-			queryGeoIp(countryDb, ipv4Address)
+			queryGeoIp(countryDb, ipv4)
 		}
 
 	}
@@ -61,14 +64,14 @@ func main() {
 	app.Run(os.Args)
 }
 
-func queryGeoIp(countryDb string, ipv4Address string) {
+func queryGeoIp(countryDb string, ipv4 string) {
 	db, err := geoip2.Open(countryDb)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer db.Close()
 
-	ip := net.ParseIP(ipv4Address)
+	ip := net.ParseIP(ipv4)
 
 	record, err := db.Country(ip)
 	if err != nil {
@@ -77,7 +80,7 @@ func queryGeoIp(countryDb string, ipv4Address string) {
 
 	countryName := record.Country.Names["en"]
 	if countryName == "" {
-		fmt.Printf("Country not found for IP: %s\n", ipv4Address)
+		fmt.Printf("Country not found for IP: %s\n", ipv4)
 	} else {
 		fmt.Printf("%s\n", record.Country.Names["en"])
 	}
